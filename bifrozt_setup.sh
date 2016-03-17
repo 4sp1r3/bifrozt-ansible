8c81163f242cfd02b827b289faa183258b14442b
Are Hansen <info@bifrozt.com>
2016-03-17 18:12:19 +0100

    bifrozt_setup.sh: version 0.0.7

--- bifrozt_setup.sh
+++ bifrozt_setup.sh
@@ -133,7 +133,7 @@ function time_stamp()
             exit 1
             ;;
         2)
-            echo -e "$time ${wht}[${end}${ylw}NOTE${end}${wht}]: $1 ${end}"
+            echo -e "$time ${wht}[${end}${ylw}INFO${end}${wht}]: $1 ${end}"
             ;;
         3)
             echo -e "$time ${wht}[${end}${blu}TASK${end}${wht}]: $1 ${end}"
@@ -207,91 +207,67 @@ function verify_clean()
     
     if [ -e "$dst_bzans" ]
     then
-        time_stamp "Removing existing \"bifrozt-ansible\" repo." "2"
-
+        time_stamp "Removing existing \"bifrozt-ansible\" repo." "3"
         rm -rf "$dst_bzans" \
         || time_stamp "Issue occured while removing old repo." "1"
-        
         time_stamp "Existing repo was removed." "0"
     fi
 
-
     if [ -e "$bz_key" ]
     then
-        time_stamp "Removing Ansible SSH keys..." "2"
-
+        time_stamp "Removing Ansible SSH keys..." "3"
         rm -rf "$bz_key" \
         || time_stamp "Encountered an issue while removing Ansible SSH keys." "1"
-
         time_stamp "Ansible SSH keys was removed." "0"
     fi
 
-
     if [ -e "/root/.ssh/authorized_keys" ]
     then
-        time_stamp "Removing /root/.ssh/authorized_keys..." "2"
-
+        time_stamp "Removing /root/.ssh/authorized_keys..." "3"
         rm "/root/.ssh/authorized_keys" \
         || time_stamp "Encountered an issue while removing \"/root/.ssh/authorized_keys\"." "1"
-
         time_stamp "The \"/root/.ssh/authorized_keys\" file was removed." "0"
     fi
 
-
     if [ -e "/etc/ansible" ]
     then
-        time_stamp "Removing Ansible from the system..." "2"
-
+        time_stamp "Removing Ansible from the system..." "3"
         apt-get remove ansible -y &>/dev/null \
         || time_stamp "Failure occured while running apt-get remove ansible." "1"
-
         apt-get purge ansible -y &>/dev/null \
         || time_stamp "Failure occured while running apt-get purge ansible." "1"
-
         rm -rf "/etc/ansible" \
         || time_stamp "Failure occured when attempting to remove \"/etc/ansible\"." "1"
-
         time_stamp "Ansible was removed from the system." "0"
     fi
 
-
     if [ -e "/etc/apt/sources.list.d/ansible-ansible-trusty.list" ]
     then
-        time_stamp "Removing Ansible PPA..." "2"
-
+        time_stamp "Removing Ansible PPA..." "3"
         add-apt-repository --remove ppa:ansible/ansible -y &>/dev/null \
         || time_stamp "Encountered an issue while removing Ansible PPA from the system." "1"
-
         rm "/etc/apt/sources.list.d/ansible-ansible-trusty.list" \
         || time_stamp "Encountered an issue while removing Ansible PPA from the system." "1"
-
         time_stamp "Ansible PPA was removed from the system." "0"
     fi
-
 
     case "$1" in
         startup)
             if [ -e "/etc/geoip2" ]
             then
-                time_stamp "Removing \"/etc/geoip2\"..." "2"
-
+                time_stamp "Removing \"/etc/geoip2\"..." "3"
                 rm -rf "/etc/geoip2" \
                 || time_stamp "Failed to remove \"/etc/geoip2\"." "1"
-
                 time_stamp "Removed \"/etc/geoip2\"." "0"
             fi
 
-
             if [ -e "$honssh_dir" ]
             then
-                time_stamp "Removing $honssh_dir..." "2"
-
+                time_stamp "Removing $honssh_dir..." "3"
                 rm -rf "$honssh_dir" \
                 || time_stamp "Issue while removing Ansible PPA from the system." "1"
-
                 time_stamp "$honssh_dir was removed." "0"
             fi
-
 
             time_stamp "Pre-execution clean up completed." "0"
             ;;
@@ -306,71 +282,50 @@ function verify_clean()
 # Update system, add Ansible PPA, install ansible and configure Ansible host key checking.
 function apt_get_things()
 {
-    time_stamp "Installing all avalible system updates..." "2"
-
+    time_stamp "Installing all avalible system updates..." "3"
     apt-get update &>/dev/null \
     || time_stamp "Failure occured while running apt-get update." "1"
-
     apt-get upgrade -y &>/dev/null \
     || time_stamp "Failure occured while running apt-get upgrade." "1"
-
     time_stamp "All avalible system updates have been installed." "0"
 
-
-    time_stamp "Installing some minor dependencies..." "2"
-
+    time_stamp "Installing some minor dependencies..." "3"
     apt-get install software-properties-common git openssh-server -y &>/dev/null \
     || time_stamp "Failure occured while running apt-get install software-properties-common git openssh-server." "1"
-
     time_stamp "Minor dependencies has been installed." "0"
 
-
-    time_stamp "Adding Ansible PPA... " "2"
-
+    time_stamp "Adding Ansible PPA... " "3"
     apt-add-repository ppa:ansible/ansible -y &>/dev/null \
     || time_stamp "Failure occured while running apt-add-repository ppa:ansible/ansible." "1"
-
     time_stamp "Ansible PPA has been added." "0"
 
-    
-    time_stamp "Installing Ansible... " "2"
-    
+    time_stamp "Installing Ansible... " "3"    
     apt-get update &>/dev/null \
     || time_stamp "Failure occured while running apt-get update." "1"
-    
     apt-get install ansible  -y &>/dev/null \
     || time_stamp "Failure occured while running apt-get install ansible." "1"
-
     time_stamp "Ansible has been installed." "0"
-
 
     if [ ! -e "$ansible_cfg" ]
     then
         time_stamp "$ansible_cfg was not found." "1"
     else
-        time_stamp "Configuring Ansible..." "2"
-
+        time_stamp "Configuring Ansible..." "3"
         if [ ! -d "$bz_key" ]
         then
             mkdir "$bz_key" \
             || time_stamp "Failed to create $bz_key." "1"
-
             chmod 0700 "$bz_key" \
             || time_stamp "Failed to set permissions on $bz_key." "1"
-
             chown root:root "$bz_key" \
             || time_stamp "Failed to set ownership on $bz_key." "1"
         fi
-
         curr_str="#private_key_file = \/path\/to\/file"
         keys_str="private_key_file = \/etc\/ansible\/BZKEY\/id_rsa"
-
         sed -i "s/$curr_str/$keys_str/g" "$ansible_cfg" \
         || time_stamp "Failure occured when updating private_key_file location in $ansible_cfg." "1"
-
         sed -i 's/#host_key_checking/host_key_checking/g' "$ansible_cfg" \
         || time_stamp "Failure occured when updating host_key_checking in $ansible_cfg." "1"
-
         time_stamp "Ansible has been configured." "0"
     fi
 }
@@ -386,18 +341,14 @@ function git_clone()
         time_stamp "Did not receive any URL." "1"
     fi
 
-
     if [ -z "$2" ]
     then
         time_stamp "Did not receive path to local destination." "1"
     fi
 
-
     time_stamp "Grabbing a clone of $1...${rdb}DEVELOPMENT BRANCH${end}" "3"
-
     git clone -b development "$1" "$2" &>/dev/null \
     || time_stamp "git clone $1 failed." "1"
-
     time_stamp "Completed cloning of $1.${rdb}DEVELOPMENT BRANCH${end}" "0"
 }
 
@@ -405,45 +356,33 @@ function git_clone()
 # Generate SSH keys for Ansible.
 function gen_ssh_keys()
 {
-    time_stamp "Setting up Ansible SSH keys..." "2"
-
+    time_stamp "Setting up Ansible SSH keys..." "3"
     ssh-keygen -f "$bz_key/id_rsa" -t rsa -N '' &>/dev/null \
     || time_stamp "Generation of Ansible SSH keys failed." "1"
-
     time_stamp "Generation of Ansible SSH keys completed." "0"
-
 
     if [ ! -e "$bz_key/id_rsa.pub" ]
     then
         time_stamp "$bz_key/id_rsa.pub could not be located." "1"
     fi
 
-
     if [ ! -d "/root/.ssh" ]
     then
-        time_stamp "Unable to find \"/root/.ssh\", creating it now..." "2"
-
+        time_stamp "Unable to find \"/root/.ssh\", creating it now..." "3"
         mkdir "/root/.ssh" \
         || time_stamp "Failed to create \"/root/.ssh\"." "1"
-        
         chmod 0700 "/root/.ssh" \
         || time_stamp "Failed to set permissions on \"/root/.ssh\"." "1"
-        
         chown -R root:root "/root/.ssh" \
         || time_stamp "Failed to set ownership on \"/root/.ssh\"." "1"
-        
         time_stamp "\"/root/.ssh\" was created." "0"
     fi
 
-
-    time_stamp "Setting up authentication key for root user (will be removed later)..." "2"
-
+    time_stamp "Setting up authentication key for root user (will be removed later)..." "3"
     cat "$bz_key/id_rsa.pub" > "/root/.ssh/authorized_keys" \
     || time_stamp "Failed to create \"/root/.ssh/authorized_keys\"." "1"
-
     chmod 0600 "$bz_key/id_rsa.pub" \
     || time_stamp "Failed to set permissions on \"$bz_key/id_rsa.pub\"." "1"
-
     time_stamp "Authentication key for root has been created." "0"
 
     #time_stamp "Updating private_key_file location in $ansible_cfg." "2"
@@ -463,32 +402,26 @@ function run_play()
         time_stamp "Did not receive absoloute path to playbook.yml." "1"
     fi
 
-
     if [ ! -e "$1" ]
     then
         time_stamp "\"$1\" does not appear to exist." "1"
     fi
-
 
     if [ -z "$2" ]
     then
         time_stamp "Did not receive path to Ansible host file." "1"
     fi
 
-
     if [ ! -e "$2" ]
     then
         time_stamp "\"$2\" does not appear to exist." "1"
     fi
 
-
     IPV4="$(ipv4_if eth0)"
     sed -i "s/IPv4_OR_FQDN/$IPV4/g" "$2"
 
-    time_stamp "Executing the playbook now..." "2"
-
+    time_stamp "Executing the playbook now..." "3"
     echo -e "${wht}" ; ansible-playbook "$1" -i "$2" ; echo -e "${end}"
-
     time_stamp "Playbook execution completed." "0"
 }
 
@@ -525,95 +458,66 @@ function setup_dhcp()
     curr_net="$(locate_current_network)"
     new_net="$(gen_new_network)"
 
-
     if [ "$(ipv4_if eth0 | cut -d '.' -f1-3)" = "$new_net" ]
     then
         new_net="$(gen_new_network)"
     fi
 
-
-    time_stamp "Randomizing the IPv4 address of the honeypot..." "2"
+    time_stamp "Generating IPv4 address of the honeypot..." "3"
 
     old_honey="$curr_net.200"
     new_honey="$curr_net.$(jot -r 1 2 254)"
 
     sed -i "s/$old_honey/$new_honey/g" "$dhcpd_conf" \
     || time_stamp "Something went wrong while randomizing or updating the IPv4 address of the honeypot." "1"
-
-    time_stamp "IPv4 address of the honeypot has been randomized and updated." "0"
-
+    time_stamp "IPv4 address for the honeypot has been created." "0"
 
     old_mac="00:22:3f:e3:1f:bf"
     new_mac="$1"
 
-
-    time_stamp "Updating the MAC address of the honeypot..." "2"
-
+    time_stamp "Updating the MAC address of the honeypot..." "3"
     sed -i "s/$old_mac/$new_mac/g" "$dhcpd_conf" \
     || time_stamp "Failed to update the MAC address of the honeypot." "1"
+    time_stamp "MAC address of the honeypot has been updated." "0"
 
-    time_stamp "MAC address of the honeypot was updated." "0"
-
-
-    time_stamp "Generating new DHCP network..." "2"
-
+    time_stamp "Generating new DHCP network..." "3"
     sed -i "s/$curr_net/$new_net/g" "$dhcpd_conf" \
     || time_stamp "Failed to generate new DHCP network." "1"
+    time_stamp "Network for the DHCP server has been created." "0"
 
-    time_stamp "DHCP will be using this network: $new_net.0/24" "3"
-
-
+    time_stamp "DHCP will be using this network: ${ylw}$new_net.0/24${end}" "2"
     new_honey_ip="$(honey_ip)"
-    time_stamp "The honeypot will be assigned this IPv4 address: $new_honey_ip" "3"
+    time_stamp "The honeypot will be assigned this IPv4 address: ${ylw}$new_honey_ip${end}" "2"
 
-
-    time_stamp "Updating configuration for interface eth1..." "2"
-
+    time_stamp "Updating configuration for interface eth1..." "3"
     sed -i "s/$curr_net/$new_net/g" "$interfaces" \
     || time_stamp "Failed to update interface configuration for eth1." "1"
-
     time_stamp "Configuration for eth1 has been updated." "0"
-
-
-    time_stamp "Updating honssh.cfg.default..." "2"
-
-    sed -i "s/$old_honey/$new_honey/g" "$hs_default_cfg" \
-    || time_stamp "Update of honssh.cfg.default failed." "1"
-
-    sed -i "s/$curr_net/$new_net/g" "$hs_default_cfg" \
-    || time_stamp "Update of honssh.cfg.default failed." "1"
-
-    time_stamp "Update of honssh.cfg.default completed." "0"
-
-
-    time_stamp "Making active configuration file, honssh.cfg, from honssh.cfg.default..." "2"
-
-    cp "$hs_default_cfg" "$hs_active_cfg" \
-    || time_stamp "Creation of active configuration file failed." "1"
-
-    time_stamp "Creation of active configuration file completed." "0"
-
-
-    time_stamp "Restarting the eth1 interface..." "2"
-
+    time_stamp "Restarting the eth1 interface..." "3"
     ifdown eth1 &>/dev/null
-
     ifup eth1 &>/dev/null \
     || time_stamp "Restart of interface eth1 failed." "1"
-
     time_stamp "Restart of interface eth1 completed." "0"
 
-
     eth1_ip="$(ipv4_if eth1)"
-    time_stamp "IPv4 address of eth1: $eth1_ip" "3"
-
+    time_stamp "IPv4 address of eth1: ${ylw}$eth1_ip${end}" "3"
 
     time_stamp "Restarting DHCP server..." "3"
-
     service isc-dhcp-server restart &>/dev/null \
     || time_stamp "DHCP server failed to restart." "1"
-
     time_stamp "DHCP server has been restarted." "0"
+
+    time_stamp "Updating honssh.cfg.default..." "3"
+    sed -i "s/$old_honey/$new_honey/g" "$hs_default_cfg" \
+    || time_stamp "Update of honssh.cfg.default failed." "1"
+    sed -i "s/$curr_net/$new_net/g" "$hs_default_cfg" \
+    || time_stamp "Update of honssh.cfg.default failed." "1"
+    time_stamp "Update of honssh.cfg.default completed." "0"
+
+    time_stamp "Making active configuration file, honssh.cfg, from honssh.cfg.default..." "3"
+    cp "$hs_default_cfg" "$hs_active_cfg" \
+    || time_stamp "Creation of active configuration file failed." "1"
+    time_stamp "Creation of active configuration file completed." "0"
 }
 
 
@@ -625,55 +529,40 @@ function conf_new_ssh()
         time_stamp "$sshd_conf was not found." "1"
     fi
 
-
     if [ ! -e "$ipv4_hater" ]
     then
         time_stamp "No firewall rule set was not found." "1"
     fi
 
-
-    time_stamp "Selecting a new SSH port for Bifrozt administration..." "2"
+    time_stamp "Selecting a new SSH port for Bifrozt administration..." "3"
 
     new_ssh_port="$(jot -r 1 49152 65535)"
     old_ssh_port="$(get_ssh_port)"
 
-    time_stamp "Updating sshd_config..." "2"
-
+    time_stamp "Updating sshd_config..." "3"
     sed -i "s/$old_ssh_port/$new_ssh_port/g" "$sshd_conf" \
     || time_stamp "Error occured while updating $sshd_conf." "1"
-
     time_stamp "$sshd_conf has been updated." "0"
-
 
     new_fw_ssh="-A INPUT -i eth0 -p tcp -m tcp --dport $new_ssh_port -j ACCEPT"
     curr_fw_ssh="-A INPUT -i eth0 -p tcp -m tcp --dport $old_ssh_port -j ACCEPT"
 
-
-    time_stamp "Updating firewall rules..." "2"
-
+    time_stamp "Updating firewall rules..." "3"
     sed -i "s/$curr_fw_ssh/$new_fw_ssh/g" "$ipv4_hater" \
     || time_stamp "Error occured while updating the filrewall rules." "1"
-
     time_stamp "Firewall rules has been updated." "0"
 
-    time_stamp "Restarting SSH server..." "2"
-
+    time_stamp "Restarting SSH server..." "3"
     service ssh stop &>/dev/null
-
     service ssh start &>/dev/null \
     || time_stamp "SSH server failed to start" "1"
-
     time_stamp "SSH server was restarted." "0"
 
+    time_stamp "The SSH server is now running on TCP port: ${ylw}$new_ssh_port${end}" "2"
 
-    time_stamp "The SSH server is now running on TCP port: ${grn}$new_ssh_port${end}" "3"
-
-
-    time_stamp "Applying new firewall rules..." "2"
-
+    time_stamp "Applying new firewall rules..." "3"
     iptables-restore < "$ipv4_hater" \
     || time_stamp "The new firewall rules failed to load." "1"
-
     time_stamp "The new firewall rules was applied." "0"
 }
 
@@ -707,19 +596,16 @@ function honey_ip()
 # Wrap up function.
 function wrap_up()
 {
-    echo -e "\n\n${red}=================================================${end}\n"
-
-    echo -e "${wht}Post setup actions:${end}"
-    echo -e "    ${wht}1) If the honeypot is running, power it off now.${end}"
-    echo -e "    ${wht}2) Reboot this machine.${end}"
-
     eth0_ip="$(ipv4_if eth0)"
     ssh_port="$(get_ssh_port)"
 
+    echo -e "\n\n${red}=================================================${end}\n"
+    echo -e "${wht}Post setup actions:${end}"
+    echo -e "    ${wht}1) If the honeypot is running, power it off now.${end}"
+    echo -e "    ${wht}2) Reboot this machine.${end}"
     echo -e "    ${wht}3) Reconnect:${end} ${ylw}ssh -l [name of user] $eth0_ip -p $ssh_port ${end}"
     echo -e "    ${wht}4) Start the honeypot.${end}"
     echo -e "    ${wht}5) Start HonSSH:${end} ${ylw}sudo honsshctrl start.${end}"
-
     echo -e "\n${red}=================================================${end}\n\n"
 
     rm "$0"
@@ -731,23 +617,21 @@ function env_checks()
 {
     if [ "$(id -u)" = "0" ]
     then
-        time_stamp "Are we root?...${grn}Yes${end}" "0"
+        time_stamp "Are we root?...${grn}Yes${end}." "0"
     else
         time_stamp "$Script must be executed as root or with root privileges." "1"
     fi
 
-
     if [ "$(check_distro)" = "Ubuntu" ]
     then
-        time_stamp "Are we running Ubuntu?...${grn}Yes${end}" "0"
+        time_stamp "Are we running Ubuntu?...${grn}Yes${end}." "0"
     else
         time_stamp "Ubuntu is required in order to run $Script." "1"
     fi
 
-
     if [ "$(check_if)" -ge "2" ]
     then
-        time_stamp "Do we have two network interface cards?...${grn}Yes${end}" "0"
+        time_stamp "Do we have two network interface cards?...${grn}Yes${end}." "0"
     else
         time_stamp "This machine has less than two network interface cards. $Script expected to find \"eth0\" and \"eth1\"." "1"
     fi
@@ -758,25 +642,15 @@ function env_checks()
 function main()
 {
     script_banner
-
     verify_clean "startup"
-
     env_checks
-
     apt_get_things
-
     git_clone "$git_bzans" "$dst_bzans"
-
     gen_ssh_keys "$bz_key"
-
     run_play "$dst_bzans/playbook.yml" "$dst_bzans/hosts"
-
     setup_dhcp "$1"
-
     conf_new_ssh
-
     verify_clean
-
     wrap_up
 }
 
@@ -790,4 +664,3 @@ fi
 
 
 exit 0
-
